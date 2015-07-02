50642a8d4b143fad5a871dceec2fd21d3096b556
Chinmay <chinu.anaokar@gmail.com>
2015-07-03 01:40:51 +0530

    v2 of App Added Hourly and Daily Modules

---
+++ app/src/main/java/com/chocodroid/stormy/DailyForecastActivity.java
@@ -0,0 +1,30 @@
+package com.chocodroid.stormy;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+
+import com.chocodroid.stormy.adapters.DayAdapter;
+import com.chocodroid.stormy.ui.MainActivity;
+import com.chocodroid.stormy.weather.Day;
+
+import java.util.Arrays;
+
+
+public class DailyForecastActivity extends ListActivity {
+
+    private Day[] mDays;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_forecast);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
+        mDays = Arrays.copyOf(parcelables,parcelables.length,Day[].class);
+        DayAdapter adapter = new DayAdapter(this, mDays);
+        this.setListAdapter(adapter);
+    }
+}
